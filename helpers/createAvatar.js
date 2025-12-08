20d380691f2d6afa86f282f8123eb1044dace59b
seliverstova-valerya <seliverstova.valerya@gmail.com>
2025-12-09 01:01:07 +0200

    chore: add unique pictures in gravatar

--- helpers/createAvatar.js
+++ helpers/createAvatar.js
@@ -1,7 +1,11 @@
 import gravatar from "gravatar";
 
 const createAvatar = (email) => {
-  return gravatar.url(email);
+  return gravatar.url(email, {
+    s: "250", // size (px)
+    d: "identicon", // unique auto-generated avatar
+    protocol: "https", // force https
+  });
 };
 
 export default createAvatar;
